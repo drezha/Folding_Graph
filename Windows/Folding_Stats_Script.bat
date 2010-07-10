18c3b90a0928253dbdedaccea1eef396c1bd2c59
Chris <drezha@csalter.me.uk>
2010-07-10 12:31:43 +0100

    Batch File

---
+++ Windows/Folding_Stats_Script.bat
@@ -0,0 +1,53 @@
+::::::::::::::::::::::::::::::::::::::::::::
+:: Personal Folding at Home Stats Script
+:: Written by - Chris Salter (aka Drezha)
+:: Last Updated - 23/04/10
+::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+::Set Variables
+set EOC=xxxxxx
+set Host=hostname.me.uk
+
+::Set Date
+for /f "tokens=1-3 delims=/ " %%a in ('date /T') do set year=%%c
+for /f "tokens=1-3 delims=/ " %%a in ('date /T') do set month=%%b
+for /f "tokens=1-3 delims=/ " %%a in ('date /T') do set day=%%a
+set temp=%year%-%month%-%day%
+
+::Backup Yesterday's Data
+copy data.dat .\Backup\data_%temp%.dat
+
+::Download data from EOC
+wget http://folding.extremeoverclocking.com/csv/user_csv.php?u=504296
+
+::Rename the file to a Windows compatible name
+rename "user_csv.php@u=%EOC%" "user.csv"
+
+::Remove the column headers
+sed /TimeStamp/d user.csv > output.csv
+
+::Calculate work unit averages
+Averages.exe
+
+::Replace comma's with tabs so that gnuplot can read the data
+sed s/,/\t/g output.csv > data.dat
+
+::Delete the user.csv so that next run the file can be renamed
+del "user.csv"
+
+::Tail to get the last 30 lines of data from data.dat
+::Used to create summary graph for the last 30 day of returns
+::Needs the Windows Server 2003 Resource Kit Tools for tail - available from Microsoft
+tail -30 data.dat > 30_Day_Data.dat
+
+::Create gnuplot graphs
+::Relies on gnuplot path being added to windows path - in my case C:\Program Files\gnuplot\binary
+gnuplot Points.plt
+gnuplot Points_Team_Place.plt
+gnuplot Points_WUs.plt
+gnuplot Team_Place.plt
+gnuplot Average_WU.plt
+gnuplot Overall.plt
+
+::Upload data to Webserver
+ftp -s:FTP.txt %Host%
